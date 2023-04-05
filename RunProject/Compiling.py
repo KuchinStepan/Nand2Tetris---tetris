4c79fb21e5946e6c3dd9cdf93f0b222c4f71c47b
ninimimm <qwertymp1011@gmail.com>
2023-04-05 09:17:28 +0500

    Fix some bugs

--- RunProject/Compiling.py
+++ RunProject/Compiling.py
@@ -1,4 +1,7 @@
-import subprocess
-subprocess.run(["python","clearVM.py"])
-subprocess.run(["python","clear.py"])
-subprocess.run(["cmd","/c", "RunTetris.bat"], shell=True)
+import subprocess, os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, "nand2tetris", "tools", "VMEmulator.bat")
+subprocess.run(["python", "clearVM.py"])
+subprocess.run(["python", "clear.py"])
+subprocess.run(["cmd", "/c", "RunTetris.bat"], shell=True)
+subprocess.run(["cmd", "/c", file_path], shell=True)
